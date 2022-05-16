d36c39a62d8b257591396ec8295551f5c195a418
Leo Durrant <poweredbykurisu@hotmail.com>
2022-05-16 16:54:27 +0100

    Added dark theme + corrected material icons import

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import MainCard from './MainCard'
 import './index.css'
-import 'material-icons/css/material-icons.scss'
+import './DarkTheme.scss'
 import './materialize-src/sass/materialize.scss'
+import 'material-icons/iconfont/material-icons.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
