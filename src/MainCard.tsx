e79fdddfb819a13b443c7f5c569849f095d97981
Leo Durrant <poweredbykurisu@hotmail.com>
2022-05-16 17:19:39 +0100

    Added profile points

--- src/MainCard.tsx
+++ src/MainCard.tsx
@@ -1,6 +1,5 @@
 import './materialize-src/sass/materialize.scss'
 import './MainCard.scss'
-import 'https://fonts.googleapis.com/icon?family=Material+Icons'
 
 function MainCard() {
 
@@ -8,21 +7,27 @@ function MainCard() {
     <div className="container">
       <div className="row">
         <div className="main col s6 offset-s3">
-          <div className="card vertical">
+          <div className="card hoverable vertical">
             <div className="card-image">
-              <img src="https://github.com/ZanyLeonic.png?size=460" width="10%" height="10%" />
+              <img className="circle responsive-img" src="https://github.com/chazzox.png?size=460" width="10%" height="10%" />
             </div>
             <div className="card-stacked">
               <div className="card-content">
-                <span className="card-title">Leo Durrant</span>
-                <span className="card-subtitle">Other Aliases: Leonic, ZanyLeonic</span>
-                <span><i className='material-icons'>school</i></span>
+                <div className="card-header">
+                  <span className="card-title">Leo Durrant</span>
+                  <span className="card-subtitle">Other Aliases: Leonic, ZanyLeonic</span>
+                </div>
+                <div className="divider"></div>
+                <div className="extra-info">
+                  <div className="profile-point"><span><i className="material-icons">school</i> BSc Computer Science (With a Year in Industry) @ RHUL</span></div>
+                  <div className="profile-point"><span><i className="material-icons">location_city</i> Egham, Surrey, United Kingdom</span></div>
+                </div>
               </div>
               <div className="card-action">
-                <div className="service"><span>CV</span><a className="right" href="#">View</a></div>
-                <div className="service"><span>Portfolio</span><a className="right" href="#">View</a></div>
-                <div className="service"><span>GitHub</span><a className="right" href="#">ZanyLeonic</a></div>
-                <div className="service"><span>LinkedIn</span><a className="right" href="#">Leo Durrant</a></div>
+                <div className="profile-point"><span>CV</span><a className="right" href="#">View</a></div>
+                <div className="profile-point"><span>Portfolio</span><a className="right" href="#">View</a></div>
+                <div className="profile-point"><span>GitHub</span><a className="right" href="#">ZanyLeonic</a></div>
+                <div className="profile-point"><span>LinkedIn</span><a className="right" href="#">Leo Durrant</a></div>
               </div>
             </div>
           </div>
