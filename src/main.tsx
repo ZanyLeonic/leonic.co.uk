60cb7fcc2f25fa8fd8ec7b68845a05fabfef0aed
Leo Durrant <poweredbykurisu@hotmail.com>
2022-05-16 17:19:51 +0100

    Added component to change the title

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,18 @@ import './DarkTheme.scss'
 import './materialize-src/sass/materialize.scss'
 import 'material-icons/iconfont/material-icons.scss'
 
+class Main extends React.Component {
+  componentDidMount() {
+    document.title = "Leo Durrant | leonic.co.uk"
+  }
+  render(){
+    return("")
+  }
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+      <Main />
       <MainCard />
   </React.StrictMode>
 )
