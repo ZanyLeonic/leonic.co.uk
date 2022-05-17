5b90912b8326b942c84ca499a92d89501355ef9b
Leo Durrant <poweredbykurisu@hotmail.com>
2022-05-17 22:51:44 +0100

    Changed index.css to sass

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import MainCard from './MainCard'
-import './index.css'
+import './index.scss'
 import './DarkTheme.scss'
 
 import 'material-icons/iconfont/material-icons.css'
 
-class Main extends React.Component {
-  componentDidMount() {
-    document.title = "Leo Durrant | leonic.co.uk"
-  }
-  render(){
-    return("")
-  }
-}
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-      <Main />
       <div className="container">
         <div className="row">
           <MainCard />
