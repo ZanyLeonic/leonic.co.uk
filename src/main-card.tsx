845137e904778ef1fb7f46256975920a6d3d5e69
Leo Durrant <poweredbykurisu@hotmail.com>
2022-05-18 15:57:44 +0100

    Added email + moved CSS rules

--- src/main-card.tsx
+++ src/main-card.tsx
@@ -63,7 +63,8 @@ class MainCard extends Component<MainCardProps, LoadingState> {
               <div className="extra-info">
                 <div className="profile-point desktop"><span><i className="material-icons">school</i> BSc Computer Science (With a Year in Industry) @ RHUL</span></div>
                 <div className="profile-point mobile"><span><i className="material-icons">school</i> BSc Computer Science (YINI) @ RHUL</span></div>
-                <div className="profile-point "><span><i className="material-icons">location_city</i> Egham, Surrey, United Kingdom</span></div>
+                <div className="profile-point"><span><i className="material-icons">location_city</i> Egham, Surrey, United Kingdom</span></div>
+                <div className="profile-point"><span><i className="material-icons">contact_mail</i> <a href="mailto:admin@leonic.co.uk">admin@leonic.co.uk</a></span></div>
               </div>
             </div>
             <div className="card-action">
