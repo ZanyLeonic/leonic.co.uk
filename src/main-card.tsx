47db627043b15d52d101b8db9f7f8ad0bb18d27e
Leo Durrant <poweredbykurisu@hotmail.com>
2022-05-21 21:14:20 +0100

    Added defined width and height

--- src/main-card.tsx
+++ src/main-card.tsx
@@ -86,7 +86,13 @@ class MainCard extends Component<MainCardProps, LoadingState> {
                 <ImagePlaceholder />
               ) : (
                 <FadeIn>
-                  <img className="responsive-img" alt={this.props.gitHubName + "'s profile picture"} src={this.state.imageURL} />
+                  <img 
+                  className="responsive-img" 
+                  alt={this.props.gitHubName + "'s profile picture"} 
+                  src={this.state.imageURL} 
+                  height="460px"
+                  width="460px"
+                  />
                 </FadeIn>
               )     
             }
