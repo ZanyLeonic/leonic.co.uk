33cd52c5ff57b44fbede58b89cb0e7a784254235
Leo Durrant <poweredbykurisu@hotmail.com>
2022-08-23 04:35:19 +0100

    chore: removed legacy ie11 support

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import legacy from '@vitejs/plugin-legacy'
-import "isomorphic-fetch"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import "isomorphic-fetch";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), legacy({
-    targets: ['defaults', 'ie 11']
-  })],
+  plugins: [react()],
   publicDir: "static",
-  
-})
+});
