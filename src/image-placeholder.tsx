e2c39eb5b1e0fd55c27b32487a92574246de28bb
Leo Durrant <poweredbykurisu@hotmail.com>
2023-05-23 21:55:29 +0100

    chore: image placeholder adjusted

--- src/image-placeholder.tsx
+++ src/image-placeholder.tsx
@@ -1,21 +1,11 @@
-import { Component } from "react";
-import ContentLoader from "react-content-loader";
+import React from "react";
 
-export default class ImagePlaceholder extends Component {
-  render() {
-    return (
-      <div className="card-image loader">
-        <ContentLoader
-          animate={true}
-          height={600}
-          speed={2}
-          foregroundColor="#f3f3f3"
-          backgroundColor="#ecebeb"
-          style={{ width: "100%", height: "100%" }}
-        >
-          <rect x="0" y="0" rx="0" ry="0" width="700" height="700" />
-        </ContentLoader>
-      </div>
-    );
-  }
+const ImagePlaceholder = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 460 460">
+      <rect x="0" y="0" rx="0" ry="0" width="0" height="0" />
+    </svg>
+  );
 }
+
+export default ImagePlaceholder;
