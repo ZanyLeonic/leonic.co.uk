a11d1cd6bef120ed723b1ce22b8b4d22da67f47f
Leo Durrant <poweredbykurisu@hotmail.com>
2023-08-13 21:47:38 +0100

    wip: vite importer settings

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  assetsInclude: ['**/*.md'],
   plugins: [react()],
   publicDir: "static",
   resolve: {
