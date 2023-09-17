b4ac943be52e63bdbaf274139cf3e5f40586c687
Leo Durrant <poweredbykurisu@hotmail.com>
2023-09-17 19:11:37 +0100

    feat: migrate home to function based component

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { LocalStorage } from "ttl-localstorage";
 import parse from "html-react-parser";
@@ -9,157 +9,129 @@ import { createBlobFromImage, fetchImageFromCache, getJSON } from "@/util";
 import config from "@/config.json";
 import "@/sass/home.scss";
 
-interface HomeState {
-  loading: boolean;
-  imageURL: string;
-  bio: string;
-}
-
 const avatarCacheID = "avatar";
 const apiCacheID = "api";
 
-class Home extends Component<{}, HomeState> {
-  constructor(props: {}) {
-    super(props);
-    this.state = {
-      loading: true,
-      imageURL: "",
-      bio: "(loading...)",
-    };
-  }
+const Home = () => {
+  const [loading, setLoading] = useState(true);
+  const [imageURL, setImageURL] = useState("");
+  const [bio, setBio] = useState("(loading...)");
 
-  componentDidMount() {
+  useEffect(() => {
+    async function fetchUserInfo(cachedResponse: string) {
+      const response =
+        cachedResponse == null
+          ? await getJSON(
+            `https://api.github.com/users/${config.user_info.github}`
+          )
+          : cachedResponse;
+
+      createBlobFromImage(response.avatar_url, avatarCacheID, 43200).then(
+        (imageBlobURL) => {
+          LocalStorage.put(apiCacheID, response, 43200);
+
+          setImageURL(imageBlobURL);
+          setBio(response.bio);
+          setLoading(false);
+        }
+      );
+    }
     // Load the cached data from local storage
     const cachedResponse = LocalStorage.get(apiCacheID);
     const restoredImage = fetchImageFromCache(avatarCacheID);
 
     // If the API call hasn't been made in the last 12 hours, refetch the data
     if (cachedResponse == null || restoredImage == "") {
-      this.fetchUserInfo(cachedResponse);
+      fetchUserInfo(cachedResponse);
     } else {
       // Use the cached data
-      this.setState({
-        loading: false,
-        imageURL: restoredImage,
-        bio: cachedResponse.bio,
-      });
+      setImageURL(restoredImage);
+      setBio(cachedResponse.bio);
+      setLoading(false);
     }
     document.title = "Leo Durrant | leonic.co.uk";
-  }
+  }, []);
 
-  async fetchUserInfo(cachedResponse: string) {
-    const response =
-      cachedResponse == null
-        ? await getJSON(
-          `https://api.github.com/users/${config.user_info.github}`
-        )
-        : cachedResponse;
-
-    createBlobFromImage(response.avatar_url, avatarCacheID, 43200).then(
-      (imageBlobURL) => {
-        LocalStorage.put(apiCacheID, response, 43200);
-
-        this.setState({
-          loading: false,
-          imageURL: imageBlobURL,
-          bio: response.bio,
-        });
-      }
-    );
-  }
-
-  render() {
-    return (
-      <MainCard>
-        <div className="home-wrapper max-w-xl md:max-w-2xl" id="home-wrapper" data-content="home">
-          {this.state.loading ? (
-            <div className="progress">
-              <div className="indeterminate"></div>
-            </div>
-          ) : null}
-          <div className="card-image">
-            {this.state.loading ? (
-              <div className="h-[460px] w-[460px]"
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center"
-                }}>
-                <div className="preloader-wrapper active center">
-                  <div className="spinner-layer spinner-red-only">
-                    <div className="circle-clipper left">
-                      <div className="circle"></div>
-                    </div><div className="gap-patch">
-                      <div className="circle"></div>
-                    </div><div className="circle-clipper right">
-                      <div className="circle"></div>
-                    </div>
+  return (
+    <MainCard>
+      <div className="home-wrapper max-w-xl md:max-w-2xl" id="home-wrapper" data-content="home">
+        {loading ? (
+          <div className="progress">
+            <div className="indeterminate"></div>
+          </div>
+        ) : null}
+        <div className="card-image">
+          {loading ? (
+            <div className="h-[460px] w-[460px]"
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center"
+              }}>
+              <div className="preloader-wrapper active center">
+                <div className="spinner-layer spinner-red-only">
+                  <div className="circle-clipper left">
+                    <div className="circle"></div>
+                  </div><div className="gap-patch">
+                    <div className="circle"></div>
+                  </div><div className="circle-clipper right">
+                    <div className="circle"></div>
                   </div>
                 </div>
               </div>
-            ) : (
-              <img
-                className="profile-image h-1/5"
-                alt={config.user_info.github + "'s profile picture"}
-                src={this.state.imageURL}
-              />
-            )}
-          </div>
-          <div className="home card-stacked">
-            <div className="card-content">
-              <div className="card-header">
-                <span className="card-title">{config.user_info.name}</span>
-                <span className="card-subtitle">
-                  Other Aliases: {config.user_info.aliases}
-                </span>
-              </div>
-              <div className="divider"></div>
-              <div className="extra-info">
-                {config["profile-points"].map((point, i) => {
-                  return (
-                    <div className="profile-point" key={i}>
-                      <span>
-                        <i className="material-icons">{point.icon}</i>{" "}
-                        {parse(point.content)}
-                      </span>
-                    </div>
-                  );
-                })}
-                {this.state.bio != null ? (
-                  <div className="profile-point">
+            </div>
+          ) : (
+            <img
+              className="profile-image h-1/5"
+              alt={config.user_info.github + "'s profile picture"}
+              src={imageURL}
+            />
+          )}
+        </div>
+        <div className="home card-stacked">
+          <div className="card-content">
+            <div className="card-header">
+              <span className="card-title">{config.user_info.name}</span>
+              <span className="card-subtitle">
+                Other Aliases: {config.user_info.aliases}
+              </span>
+            </div>
+            <div className="divider"></div>
+            <div className="extra-info">
+              {config["profile-points"].map((point, i) => {
+                return (
+                  <div className="profile-point" key={i}>
                     <span>
-                      <i className="material-icons">info_outline</i> "
-                      <i>{this.state.bio}</i>"
+                      <i className="material-icons">{point.icon}</i>{" "}
+                      {parse(point.content)}
                     </span>
                   </div>
-                ) : (
-                  <></>
-                )}
-              </div>
+                );
+              })}
+              {bio != null ? (
+                <div className="profile-point">
+                  <span>
+                    <i className="material-icons">info_outline</i> "
+                    <i>{bio}</i>"
+                  </span>
+                </div>
+              ) : (
+                <></>
+              )}
             </div>
-            <div className="card-action">
-              <table>
-                <tbody>
-                  {config.links.map((link, i) => {
-                    return (
-                      <tr key={i}>
-                        <td>{link.title}</td>
-                        <td className="right">
-                          {link.nav_link ? (
-                            <Link to={link.url}>
-                              <a
-                                href="#"
-                                className={
-                                  "waves-effect waves-linkColour btn-flat" +
-                                  (!link.enabled ? " disabled" : "")
-                                }
-                              >
-                                {link.button_text}
-                              </a>
-                            </Link>
-                          ) : (
+          </div>
+          <div className="card-action">
+            <table>
+              <tbody>
+                {config.links.map((link, i) => {
+                  return (
+                    <tr key={i}>
+                      <td>{link.title}</td>
+                      <td className="right">
+                        {link.nav_link ? (
+                          <Link to={link.url}>
                             <a
-                              href={link.url}
+                              href="#"
                               className={
                                 "waves-effect waves-linkColour btn-flat" +
                                 (!link.enabled ? " disabled" : "")
@@ -167,19 +139,29 @@ class Home extends Component<{}, HomeState> {
                             >
                               {link.button_text}
                             </a>
-                          )}
-                        </td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
-            </div>
+                          </Link>
+                        ) : (
+                          <a
+                            href={link.url}
+                            className={
+                              "waves-effect waves-linkColour btn-flat" +
+                              (!link.enabled ? " disabled" : "")
+                            }
+                          >
+                            {link.button_text}
+                          </a>
+                        )}
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
           </div>
         </div>
-      </MainCard>
-    );
-  }
+      </div>
+    </MainCard>
+  );
 }
 
 export default Home;
